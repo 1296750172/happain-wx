784d58adda188837bc22a467cdbb1868a73fd0dd
happain <1296750172@qq.com>
2022-04-11 03:09:02 +0800

    完成第一版wx工程.0

--- happain.sh
+++ happain.sh
@@ -1,8 +1,5 @@
 pwd
 kill -9 $(lsof -i:80 -t)
-
-#/usr/local/maven3.8.5/bin/mvn clean
-#/usr/local/maven3.8.5/bin/mvn package
+/usr/local/maven3.8.5/bin/mvn clean
+/usr/local/maven3.8.5/bin/mvn package
 nohup java -jar ./target/wx-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
-
-
