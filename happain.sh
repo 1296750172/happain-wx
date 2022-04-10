dcbd116bf57cf1760ebdd9c3106cc8dba391c76f
happain <1296750172@qq.com>
2022-04-11 03:03:50 +0800

    1111111111

--- happain.sh
+++ happain.sh
@@ -1,7 +1,8 @@
 pwd
+kill -9 $(lsof -i:80 -t)
 
 #/usr/local/maven3.8.5/bin/mvn clean
 #/usr/local/maven3.8.5/bin/mvn package
-nohup java -jar ./target/wx-0.0.1-SNAPSHOT.jar >> nohup.out &
+nohup java -jar ./target/wx-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
 
 
